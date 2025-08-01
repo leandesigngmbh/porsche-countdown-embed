d550b960e2de91cbb6648017febc21d119bf3e3f
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-08-01 12:15:37 +0200

    modified countdown

--- src/lib/components/countdown.tsx
+++ src/lib/components/countdown.tsx
@@ -28,6 +28,13 @@ function Countdown({ targetTime }: CountdownProps) {
     return timeLeft;
   };
 
+  const german: Record<string, string> = {
+    days: "Tage",
+    hours: "Stunden",
+    minutes: "Minuten",
+    seconds: "Sekunden",
+  };
+
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {
@@ -41,17 +48,24 @@ function Countdown({ targetTime }: CountdownProps) {
 
   return (
     <div className="flex gap-6 sm:gap-8 lg:gap-12 text-3xl sm:text-7xl lg:text-9xl font-bold">
-      {["days", "hours", "minutes", "seconds"].map((unit) => (
-        <div key={unit} className="flex flex-col items-center">
-          <AnimateNumber format={{ minimumIntegerDigits: 2 }}>
-            {String(timeLeft[unit as keyof typeof timeLeft]).padStart(2, "0")}
-          </AnimateNumber>
+      {["days", "hours", "minutes", "seconds"].map((unit) => {
+        return (
+          <div key={unit} className="flex flex-col items-center">
+            <AnimateNumber
+              className="tracking-wider"
+              format={{ minimumIntegerDigits: 2 }}
+            >
+              {String(timeLeft[unit as keyof typeof timeLeft]).padStart(2, "0")}
+            </AnimateNumber>
 
-          <span className="text-xs uppercase tracking-widest -mt-4">
-            {unit}
-          </span>
-        </div>
-      ))}
+            <div className="text-xs uppercase flex flex-col items-center lg:-mt-4">
+              <span className="">{unit}</span>
+
+              <span className="text-neutral-400">{german[unit]}</span>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
